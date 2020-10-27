2b76c8efbdcc9affbd74693570bc7bd409b78a87
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2020-10-27 15:50:36 -0500

    Create awsSQS.tf

---
+++ awsSQS.tf
@@ -0,0 +1,15 @@
+resource "aws_sqs_queue" "queue" {
+  count = length(var.name)
+  name = "${var.environment}_${var.project}_${var.name[count.index]}${var.fifo_queue == "true" ? ".fifo" : ""}"
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  delay_seconds = var.delay_seconds
+  max_message_size = var.max_message_size # 256 KB
+  message_retention_seconds = var.message_retention_seconds # 4 days
+  receive_wait_time_seconds = var.receive_wait_time_seconds
+  redrive_policy = length(var.dead_letter_queue) > 0 ? data.template_file.redrive_policy.rendered : ""
+  fifo_queue = var.fifo_queue
+}
+
+
+
+
