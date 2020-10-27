8e3bac1e12beb7dd297bba9650ec751d4f5d6c77
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2020-10-27 15:26:36 -0500

    Create lambda-function.py

---
+++ lambda-function.py
@@ -0,0 +1,22 @@
+import boto3
+s3 = boto3.resource('s3')
+sqs = boto3.client('sqs')
+queue_url = 'your_queue_url'
+def lambda_handler(s3_event, context):
+    # Get the s3 event
+    for record in s3_event.get("Records"):
+        bucket = record.get("s3").get("bucket").get("name")
+        key = record.get("s3").get("object").get("key")
+        
+        # Send message to SQS queue
+        response = sqs.send_message(
+            QueueUrl=queue_url,
+            DelaySeconds=1,
+            MessageAttributes={
+                'key': {
+                    'DataType': 'String',
+                    'StringValue': key
+                }
+            },
+            MessageBody=(bucket)
+        )
