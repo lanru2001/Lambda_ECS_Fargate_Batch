4b4c7ae4490b68b1e39b6bfd85d635c8efdb9225
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2020-10-27 14:23:20 -0500

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -1,3 +1,14 @@
 resource "aws_s3_bucket" "bucket" {
 bucket = "event_bucket_name"
 }
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = "${aws_s3_bucket.bucket.id}"
+
+  lambda_function {
+    lambda_function_arn = "${aws_lambda_function.s3lambda.arn}"
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "<_prefix_if_any_dir_in_s3>/"
+    filter_suffix       = "<_suffix_of_file_put_in_s3>"
+  }
+}
