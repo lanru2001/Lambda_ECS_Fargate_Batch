8dc8743b2ac923923b28648e9fc777c73328deba
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-12-04 13:18:56 -0600

    Update lambdafunction.tf

--- lambdafunction.tf
+++ lambdafunction.tf
@@ -3,10 +3,8 @@ resource "aws_lambda_function" "s3lambda" {
   function_name = "s3_event_lambda"
   role          = "${aws_iam_role.lambda_role.arn}"
   handler       = "lambda_function.lambda_handler"
-  runtime   = "python2.7"
+  runtime       = "python2.7"
 }
-
------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 resource "aws_iam_role" "role" {
